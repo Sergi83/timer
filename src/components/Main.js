3d447df72b8bfc04049f403929229ba101cf3a2d
Serhii Stepaniuk <serhii.stepaniuk@outlook.com>
2022-01-28 02:15:18 +0200

    create buttons (start, stop, wait, reset), hook and functions (for buttons and to count time), correct styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,105 @@
-import React from "react";
-import Home from "../pages/Home";
-
+import React, { useState, useEffect } from "react";
+import { GiPauseButton } from "react-icons/gi";
+import { VscDebugStart, VscStopCircle } from "react-icons/vsc";
 
 const Main = () => {
+  
+  // The state for our timer
+  const [timer, setTimer] = useState("00: 00: 00");
+  const [totalSeconds, setTotalSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-    // 
+  //   onClick functions
+  const toggleTimer = (e) => {
+    e.preventDefault();
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = (e) => {
+    e.preventDefault();
+    setIsActive(false);
+    setTotalSeconds(0);
+    setTimer("00: 00: 00");
+  };
+
+  
+  // counting
+  useEffect(() => {
+    let interval = null;
+
+    // ifelse
+    if (isActive) {
+      // increase count every second by 1 second
+      interval = setInterval(() => {
+        setTotalSeconds((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      // clear
+      clearInterval(interval);
+    }
+
+    // clearing function
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+
+  // get formated time (string)
+  useEffect(() => {
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds / 60) - (hours * 60));
+    let seconds = Math.floor(totalSeconds  % 60);
+
+    // get 2 digits numbers
+    let [hh, mm, ss] = [hours, minutes, seconds].map(i => ("0" + i).slice(-2));
+
+    // get limit
+    const dayLimit = 24 * 3600;
+
+    return (totalSeconds < dayLimit ? setTimer(`${hh}: ${mm}: ${ss}`) : setTimer("More than one day"));
+    
+  }, [timer, totalSeconds]);
 
   return (
-    <>
-      <Home />
-    </>
+    <div className="page">
+      <div className="container">
+        <div className="title">
+          <h1>timer</h1>
+          
+          <div id="time">
+            <span>
+              {timer} (hh:mm:ss)
+            </span>
+          </div>
+          <div>{totalSeconds} s</div>
+        </div>
+        <div className="btn-container">
+          <div
+            id="start"
+            className={`btn ${isActive ? "active" : ""}`}
+            onClick={toggleTimer}
+          >
+            {isActive ? <VscStopCircle /> : <VscDebugStart />}
+          </div>
+
+          {!isActive && totalSeconds !== 0 && (
+            <div
+              id="wait"
+              className={`btn ${
+                !isActive & (totalSeconds !== 0) ? "active" : ""
+              }`}
+              onDoubleClick={toggleTimer}
+            >
+              <GiPauseButton />
+            </div>
+          )}
+          {!isActive && totalSeconds > 0 && (
+            <div id="reset" className="btn" onClick={resetTimer}>
+              reset
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,35 @@
-import React from "react";
-import { GiPauseButton } from "react-icons/gi";
-import { VscDebugStart, VscStopCircle } from "react-icons/vsc";
+// import React, {useState, useEffect} from "react";
+// import { GiPauseButton } from "react-icons/gi";
+// import { VscDebugStart, VscStopCircle } from "react-icons/vsc";
 
-const Home = () => {
-  return (
-    <div className="page">
-      <div className="container">
-        <div className="title">
-          <h1>Timer</h1>
-          <span id="time">HH:MM:SS</span>
-        </div>
-        <div className="btn-container">
-          <div id="start" className="btn">
-            <VscDebugStart />
-          </div>
-          <div id="stop" className="btn">
-            <VscStopCircle />
-          </div>
-          <div id="wait" className="btn">
-            <GiPauseButton />
-          </div>
-          <div id="reset" className="btn">
-            reset
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Home = (props) => {
 
-export default Home;
+//   return (
+//     <div className="page">
+//       <div className="container">
+//         <div className="title">
+//           <h1>timer</h1>
+//           <span id="time">{result}</span>
+//         </div>
+//         <div className="btn-container">
+//           {/* <div id="start" className={`btn ${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+//           {isActive ? (<VscStopCircle />) : (<VscDebugStart />)}
+//           </div> */}
+//           {/* <div id="start" className="btn">
+//             <VscStopCircle />
+//           </div>
+//           <div id="wait" className="btn">
+//             <GiPauseButton />
+//           </div>
+//           <div id="reset" className="btn"  onClick={resetCount}>
+//             reset
+//           </div> */}
+
+//           
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
